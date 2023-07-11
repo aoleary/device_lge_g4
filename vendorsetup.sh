0aa4a1a948e7aeb6a2c595e157ab95b4151eeb3c
aoleary <seanm187@gmail.com>
2023-07-11 09:22:14 +0000

    G4: activate extendrom Boot Debug and Magisk preroot
    * Clone/sync extendedrom: git clone https://github.com/sfX-android/android_vendor_extendrom -b main vendor/extendrom
    
    * Activate extendedrom: https://github.com/sfX-android/android_vendor_extendrom/wiki/ENABLE_EXTENDROM
    * Activate boot debug: https://github.com/sfX-android/android_vendor_extendrom/wiki/EXTENDROM_BOOT_DEBUG
    * Activate Magisk preroot: https://github.com/sfX-android/android_vendor_extendrom/wiki/EXTENDROM_PREROOT_BOOT-NG

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,18 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export USE_NINJA=true
 export LLVM_ENABLE_THREADS=1
 
+########### extendrom section ###########
+# Enable extendrom
+export ENABLE_EXTENDROM=true
+
+#Boot Debug
+export EXTENDROM_BOOT_DEBUG=true
+export EXTENDROM_DEBUG_PATH=/persist
+export EXTENDROM_DEBUG_PATH_SIZE_FULL=200
+export EXTENDROM_DEBUG_PATH_SIZE_KERNEL=100
+
+#Magisk preroot
+EXTENDROM_PREROOT_BOOT=true
+EXTENDROM_PACKAGES=Magisk
+
+$PWD/vendor/extendrom/er.sh

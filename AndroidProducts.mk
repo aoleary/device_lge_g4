b9c13fc4af45847c95279c4214fdba62b0e74c8f
aoleary <seanm187@gmail.com>
2023-09-01 07:26:29 +0000

    AOSP-Extend the G4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,42 +20,42 @@ PRODUCT_AAPT_PREF_CONFIG := 560dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 PRODUCT_MAKEFILES += \
-	$(LOCAL_DIR)/lineage_f500_usu.mk \
-	$(LOCAL_DIR)/lineage_h810_usu.mk \
-	$(LOCAL_DIR)/lineage_h811.mk \
-	$(LOCAL_DIR)/lineage_h812_usu.mk \
-	$(LOCAL_DIR)/lineage_h815.mk \
-	$(LOCAL_DIR)/lineage_h815_usu.mk \
-	$(LOCAL_DIR)/lineage_ls991_usu.mk \
-	$(LOCAL_DIR)/lineage_us991_usu.mk \
-	$(LOCAL_DIR)/lineage_vs986_usu.mk
+	$(LOCAL_DIR)/aosp_f500_usu.mk \
+	$(LOCAL_DIR)/aosp_h810_usu.mk \
+	$(LOCAL_DIR)/aosp_h811.mk \
+	$(LOCAL_DIR)/aosp_h812_usu.mk \
+	$(LOCAL_DIR)/aosp_h815.mk \
+	$(LOCAL_DIR)/aosp_h815_usu.mk \
+	$(LOCAL_DIR)/aosp_ls991_usu.mk \
+	$(LOCAL_DIR)/aosp_us991_usu.mk \
+	$(LOCAL_DIR)/aosp_vs986_usu.mk
 
 COMMON_LUNCH_CHOICES := \
-	lineage_f500_usu-userdebug \
-	lineage_f500_usu-user \
-	lineage_f500_usu-eng \
-	lineage_h810_usu-userdebug \
-	lineage_h810_usu-user \
-	lineage_h810_usu-eng \
-	lineage_h811-userdebug \
-	lineage_h811-user \
-	lineage_h811-eng \
-	lineage_h812_usu-userdebug \
-	lineage_h812_usu-user \
-	lineage_h812_usu-eng \
-	lineage_h815-userdebug \
-	lineage_h815-user \
-	lineage_h815-eng \
-	lineage_h815_usu-userdebug \
-	lineage_h815_usu-user \
-	lineage_h815_usu-eng \
-	lineage_ls991_usu-userdebug \
-	lineage_ls991_usu-user \
-	lineage_ls991_usu-eng \
-	lineage_us991_usu-userdebug \
-	lineage_us991_usu-user \
-	lineage_us991_usu-eng \
-	lineage_vs986_usu-userdebug \
-	lineage_vs986_usu-user \
-	lineage_vs986_usu-eng
+	aosp_f500_usu-userdebug \
+	aosp_f500_usu-user \
+	aosp_f500_usu-eng \
+	aosp_h810_usu-userdebug \
+	aosp_h810_usu-user \
+	aosp_h810_usu-eng \
+	aosp_h811-userdebug \
+	aosp_h811-user \
+	aosp_h811-eng \
+	aosp_h812_usu-userdebug \
+	aosp_h812_usu-user \
+	aosp_h812_usu-eng \
+	aosp_h815-userdebug \
+	aosp_h815-user \
+	aosp_h815-eng \
+	aosp_h815_usu-userdebug \
+	aosp_h815_usu-user \
+	aosp_h815_usu-eng \
+	aosp_ls991_usu-userdebug \
+	aosp_ls991_usu-user \
+	aosp_ls991_usu-eng \
+	aosp_us991_usu-userdebug \
+	aosp_us991_usu-user \
+	aosp_us991_usu-eng \
+	aosp_vs986_usu-userdebug \
+	aosp_vs986_usu-user \
+	aosp_vs986_usu-eng
 

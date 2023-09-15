a0aaf633c0dddf7b25a4ea6ec385d465e0d75eca
aoleary <seanm187@gmail.com>
2023-09-15 12:18:10 +0000

    g4: delete built-in repos
    * The repos hardware_broadcom_libbt and system_qcom have been deprecated by most ROMs
    * The versions built into the g4-common tree are the most up to date versions
    * so delete these repos if they are synced by the ROM

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,14 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export USE_NINJA=true
 export LLVM_ENABLE_THREADS=1
 
+######### Remove built-in repos #########
+#Also delete info from manifest as there are stricter checks on manifest
+#entries otherwise build will fail as ROM will look for local libbt folder
+rm -rf hardware/broadcom/libbt
+sed -i '/libbt/d' .repo/manifests/default.xml
+rm -rf system/qcom
+######### Remove built-in repos #########
+
 ########### extendrom section ###########
 # Enable extendrom
 export ENABLE_EXTENDROM=true

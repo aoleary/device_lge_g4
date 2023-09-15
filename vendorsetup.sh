37ccde85b47e8cad509454436bf45ff0f84954ce
aoleary <seanm187@gmail.com>
2023-09-15 12:18:10 +0000

    g4: delete built-in system_qcom repo
    * The repod system_qcom has been deprecated by most ROMs
    * The version built into the g4-common tree is the most up to date version
    * so delete this repo if synced by the ROM

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,6 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export USE_NINJA=true
 export LLVM_ENABLE_THREADS=1
 
+######### Remove built-in repos #########
+rm -rf system/qcom
+######### Remove built-in repos #########

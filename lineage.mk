e65f9e7b9129476d386a264f24eb845df5da7d02
Juhyung Park <qkrwngud825@gmail.com>
2023-12-05 11:11:03 +0000

    G4: Stop shipping 32-bit Zygote
    * This saves a non-negligible amount of RAM.
    * Pixel 7 onwards also do the same, meaning that the user inconvenience is none if not minimal.

--- lineage.mk
+++ lineage.mk
@@ -15,8 +15,8 @@
 #
 DEBUG_ME += lineage.mk
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)                                                        
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)                                                
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Boot animation

1669d645939e4b4312862edd5da9e2f690a36c61
aoleary <seanm187@gmail.com>
2024-02-29 07:31:45 +0000

    g4: extendedrom cleanup
    * dont build Magisk as it interferes with banking apps
    * remove line that removes Launcher3, which is needed for gestures

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,6 @@ export LLVM_ENABLE_THREADS=1
 sed -i '/libbt/d' .repo/manifests/default.xml
 rm -rf hardware/broadcom/libbt
 rm -rf system/qcom
-# Delete Launcher3 as we add Lawnchair instead
-rm -rf packages/apps/Launcher3
 ######### Remove built-in repos #########
 
 ########### extendrom section ###########
@@ -24,7 +22,7 @@ export EXTENDROM_DEBUG_PATH_SIZE_KERNEL=500
 
 #Magisk preroot
 export EXTENDROM_PREROOT_BOOT=true
-export EXTENDROM_PACKAGES="Magisk Lawnchair-stable"
+export EXTENDROM_PACKAGES="Lawnchair-stable"
 
 #clone extendedrom if it doesnt exist
 if [ ! -d "vendor/extendrom" ]

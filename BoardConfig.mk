2b9c0f6bf3bc36e36fcb71ef98a36b16d2b52366
Aifinef <perfect.moose.prbz@mask.me>
2025-05-26 13:42:05 +0400

    g4: Add TW_MAX_BRIGHTNESS for Orange Fox display control
    - TW_MAX_BRIGHTNESS := 255 (required by orangefox.mk)
    - Fix build error: bootable/recovery/orangefox.mk:700

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,7 @@ TARGET_RECOVERY_QCOM_RTC_FIX := false
 # GRAPHIC FLAGS
 TW_THEME := portrait_hdpi
 TW_DEFAULT_BRIGHTNESS := 200
+TW_MAX_BRIGHTNESS := 255
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # when double tap is enabled this has to be set to FALSE otherwise when
 # an usb cable is connected the screen is blank for several minutes
